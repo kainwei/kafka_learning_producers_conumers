872b343606671e5d3172db669352793cf1dc30d3
kainvey@gmail.com <kainvey@gmail.com>
2020-08-12 11:32:23 +1200

    update

--- src/main/java/com/linuxacademy/ccdak/clients/ConsumerMain.java
+++ src/main/java/com/linuxacademy/ccdak/clients/ConsumerMain.java
@@ -1,9 +1,35 @@
 package com.linuxacademy.ccdak.clients;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class ConsumerMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "group1");
+        props.put("enable.auto.commit", "false");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("test_topic1", "test_topic2"));
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for(ConsumerRecord<String, String> record : records) {
+                System.out.println("key=" + record.key() + ", value=" + record.value() + ", topic=" +
+                        record.topic() + ", partition=" + record.partition() + ", offset=" + record.offset()
+                );
+            }
+            consumer.commitSync();
+        }
+
     }
 
 }
--- src/main/java/com/linuxacademy/ccdak/clients/ProducerMain.java
+++ src/main/java/com/linuxacademy/ccdak/clients/ProducerMain.java
@@ -1,9 +1,49 @@
 package com.linuxacademy.ccdak.clients;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+
 public class ProducerMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        props.put("acks", "all");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        for(int i=0; i<100; i++){
+            int partition = 0;
+            if ( i > 49 ) {
+                partition = 1;
+            }
+            ProducerRecord record =
+                    new ProducerRecord<>("test_count", partition,"count", Integer.toString(i));
+
+            producer.send(record, (RecordMetadata metadata, Exception e) -> {
+                if( e != null ) {
+                    System.out.println("Error publishing message: " + e.getMessage());
+                } else {
+                    System.out.println("Published message: key=" + record.key() +
+                            ", value=" + record.value() +
+                            ", topic=" + metadata.topic() +
+                            ", partition=" + metadata.partition() +
+                            ", offset=" + metadata.offset()
+                            );
+                }
+            });
+        }
+        producer.close();
+
+
+
     }
 
 }
